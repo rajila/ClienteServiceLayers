71b7605c5bdce150414f79b8f83882bd4b0a2e20
Ronald <rdajila@gmail.com>
2019-06-15 20:00:43 +0200

    Changes

--- ClientWeb/Scripts/Business/Task/AseguradoEdit.js
+++ ClientWeb/Scripts/Business/Task/AseguradoEdit.js
@@ -9,9 +9,13 @@ function eventHistorial(i)
 {
     var _element = $("#history_"+i);
     var _row = $("#rowhistorial_" + _element.attr('numrow'));
+    var _requestAjax = true;
 
     if (_row.attr('class') === 'hide') _row.attr('class', 'showRow');
-    else _row.attr('class', 'hide');
+    else {
+        _row.attr('class', 'hide');
+        _requestAjax = false;
+    }
 
     if (_element.attr('class') === 'glyphicon glyphicon-eye-close') _element.attr('class', 'glyphicon glyphicon-eye-open');
     else _element.attr('class', 'glyphicon glyphicon-eye-close');
@@ -30,6 +34,7 @@ function eventHistorial(i)
             success: function (response) {
                 if (response != null) {
                     console.log(response);
+                    addDataInit(i, response);
                 } else {
                     alert("Something went wrong");
                 }
@@ -41,5 +46,41 @@ function eventHistorial(i)
                 alert(response.responseText);
             }
         });
+    }
+}
+
+function addDataInit(index, data) {
+    var _data = (typeof data === 'undefined' || data === null) ? [] : data;
+    if (_data.length !== 0) $("#rowtable_" + index + " tbody").empty();
+
+    for (var i = 0; i < _data.length; i++) {
+        var _elementTR = $("<tr>");
+        _elementTR.attr('id', 'row_' + this.count);
+
+        // DNI
+        var _elementTDOne = $("<td>");
+        _elementTDOne.html(_data[i]['dni']);
+        _elementTR.append(_elementTDOne);
+
+        // Nombre
+        var _elementTDTwo = $("<td>");
+        _elementTDTwo.html(_data[i]['nombre']);
+        _elementTR.append(_elementTDTwo);
+
+        // Telefono
+        var _elementTDThree = $("<td>");
+        _elementTDThree.html(_data[i]['telefono']);
+        _elementTR.append(_elementTDThree);
+
+        // Llamar
+        var _elementTDFour = $("<td>");
+        _elementTDFour.html(_data[i]['llamar_asegurado']);
+        _elementTR.append(_elementTDFour);
+
+        var _elementTDFive = $("<td>");
+        _elementTDFive.html(_data[i]['fecha_hora']);
+        _elementTR.append(_elementTDFive);
+
+        $("#rowtable_" + index + " tbody").append(_elementTR);
     }
 }
